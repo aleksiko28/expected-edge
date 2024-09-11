da00edacbd14c9dde5efcadbf96b1d0b367a7f78
Aleksi Koivu <aleksi.koivu@mogroup.com>
2024-09-11 17:40:56 +0300

    sticky column

--- src/lib/components/ui/table/table-cell.svelte
+++ src/lib/components/ui/table/table-cell.svelte
@@ -9,10 +9,21 @@
 </script>
 
 <td
-	class={cn('p-2 align-middle [&:has([role=checkbox])]:pr-0', className)}
+	class={cn(
+		'p-2 align-middle [&:has([role=checkbox])]:pr-0 first:sticky first:left-0 first:z-10 first:bg-zinc-900 md:first:bg-transparent',
+		className
+	)}
 	{...$$restProps}
 	on:click
 	on:keydown
 >
 	<slot />
 </td>
+
+<style>
+	td:first-child {
+		position: sticky;
+		left: 0;
+		z-index: 2;
+	}
+</style>
